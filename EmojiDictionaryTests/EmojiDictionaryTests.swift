b161499046f92e5405bb5b24eea1fe7d119a38ad
stevenyu253436 <stevenyu.supreme@gmail.com>
2025-01-15 06:43:48 +0800

    Initial Commit

---
+++ EmojiDictionaryTests/EmojiDictionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDictionaryTests.swift
+//  EmojiDictionaryTests
+//
+//  Created by 游哲維 on 2025/1/15.
+//
+
+import Testing
+@testable import EmojiDictionary
+
+struct EmojiDictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
