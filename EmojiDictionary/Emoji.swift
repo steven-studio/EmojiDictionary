2241e9b4bfe159d199c9bba0a69058e9fd0693ee
stevenyu253436 <stevenyu.supreme@gmail.com>
2025-01-15 11:42:33 +0800

    first commit

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by 游哲維 on 2025/1/15.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
